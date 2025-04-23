d36422bac7b4a59a538b4c3c310bdddd7da4740b
micozhang <micozhang@localhost>
2025-04-23 14:42:34 -0500

    m

--- src/assignment9/Game.java
+++ src/assignment9/Game.java
@@ -1,62 +1,85 @@
 package assignment9;
 
 import java.awt.event.KeyEvent;
+import java.util.LinkedList;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Game {
-	
-	public Game() {
-		StdDraw.enableDoubleBuffering();
-		
-		//FIXME - construct new Snake and Food objects
-	}
-	
-	public void play() {
-		while (true) { //TODO: Update this condition to check if snake is in bounds
-			int dir = getKeypress();
-			//Testing only: you will eventually need to do more work here
-			System.out.println("Keypress: " + dir);
-			
-			/*
-			 * 1. Pass direction to your snake
-			 * 2. Tell the snake to move
-			 * 3. If the food has been eaten, make a new one
-			 * 4. Update the drawing
-			 */
-		}
-	}
-	
-	private int getKeypress() {
-		if(StdDraw.isKeyPressed(KeyEvent.VK_W)) {
-			return 1;
-		} else if (StdDraw.isKeyPressed(KeyEvent.VK_S)) {
-			return 2;
-		} else if (StdDraw.isKeyPressed(KeyEvent.VK_A)) {
-			return 3;
-		} else if (StdDraw.isKeyPressed(KeyEvent.VK_D)) {
-			return 4;
-		} else {
-			return -1;
-		}
-	}
-	
-	/**
-	 * Clears the screen, draws the snake and food, pauses, and shows the content
-	 */
-	private void updateDrawing() {
-		//FIXME
-		
-		/*
-		 * 1. Clear screen
-		 * 2. Draw snake and food
-		 * 3. Pause (50 ms is good)
-		 * 4. Show
-		 */
-	}
-	
-	public static void main(String[] args) {
-		Game g = new Game();
-		g.play();
-	}
+ 
+ private Snake snake;
+ private LinkedList<Food> foods;
+ private int score;
+ 
+ public Game() {
+  StdDraw.enableDoubleBuffering();
+  snake = new Snake();
+  foods = new LinkedList<>();
+  this.score=0;
+  for (int i = 0; i < 8; i++) {
+   foods.add(new Food());
+  }
+ }
+ 
+ public void play() {
+  while (snake.isInbounds()) {
+   int dir = getKeypress();
+   if (dir != -1) {
+    snake.changeDirection(dir);
+   }
+ 
+   snake.move();
+   
+   // Check for self-collision
+         if (snake.hasCollidedWithSelf()) {
+             System.out.println("Game Over: The snake collided with itself!");
+             break;
+         }
+         
+   for (int i = 0; i < foods.size(); i++) {
+    if (snake.eatFood(foods.get(i))) {
+     foods.get(i).randomizePosition();
+     score ++; 
+     StdDraw.text(0.5,0.5,"Score"+score);
+    }
+   }
+  
+   updateDrawing();
+  }
+  
+  System.out.println("Game Over");
+ }
+ 
+ private int getKeypress() {
+  if(StdDraw.isKeyPressed(KeyEvent.VK_W)) {
+   return 1;
+  } else if (StdDraw.isKeyPressed(KeyEvent.VK_S)) {
+   return 2;
+  } else if (StdDraw.isKeyPressed(KeyEvent.VK_A)) {
+   return 3;
+  } else if (StdDraw.isKeyPressed(KeyEvent.VK_D)) {
+   return 4;
+  } else {
+   return -1;
+  }
+ }
+ 
+ /**
+  * Clears the screen, draws the snake and food, pauses, and shows the content
+  */
+ private void updateDrawing() {
+  StdDraw.clear();
+  snake.draw();
+  for (Food food : foods) {
+   food.draw();
+  }
+  StdDraw.pause(50);
+  StdDraw.show();
+  
+ }
+ 
+ public static void main(String[] args) {
+  Game g = new Game();
+  g.play();
+ }
 }
